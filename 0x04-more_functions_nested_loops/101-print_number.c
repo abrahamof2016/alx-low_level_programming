ce91fee5e32478a3f8a89bb228db3cf530ce235f
jarageda <jarageda9@gmail.com>
2023-08-18 18:20:15 +0000

    prints an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,12 +7,14 @@
   */
 void print_number(int n)
 {
+	if (n == 0)
+		_putchar(0);
 	if (n < 0)
 	{
 		_putchar(45);
 		n = -n;
 	}
-	if (n / 10)
+	if ((n / 10) && (n  != 0))
 		print_number(n / 10);
 	_putchar(n % 10 + '0');
 }
