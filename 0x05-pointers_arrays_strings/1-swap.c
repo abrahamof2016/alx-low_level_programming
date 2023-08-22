0e103f77eec1d8edb5f613b1c0b91349a2ba2915
jarageda <jarageda9@gmail.com>
2023-08-22 13:02:25 +0000

    swaps a value

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * swap_int - swaps the values.
+  * @a: contains integer value.
+  * @b: contains integer value.
+  * Return: void.
+  */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
