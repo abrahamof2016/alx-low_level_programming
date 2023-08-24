e73ef1bf44f78b4fe22b9d5d1c8f83d74b247f8d
jarageda <jarageda9@gmail.com>
2023-08-24 13:46:03 +0000

    compared a string

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+  * _strcmp - compare two strings.
+  * @s1: a string.
+  * @s2: a string.
+  * Return: an integer.
+  */
+int _strcmp(char *s1, char *s2)
+{
+	int a = strcmp(s1, s2);
+	return (a);
+}
