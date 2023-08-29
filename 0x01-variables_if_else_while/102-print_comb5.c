5a6dbb91ad5d15eb3b9980e1cbc566ce228880bb
jarageda <jarageda9@gmail.com>
2023-08-29 14:02:05 +0000

    possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,31 +10,38 @@ int main(void)
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 8; j++)
-		{
-			for (k = 0; k <= 9; k++)
+			if (j == 9)
 			{
-				for (l = 0; l <= 9; l++)
+				for (k = i + 1; k <= 9; k++)
 				{
-					if ((i <= k) && (j < 1 || j == 0 || l == 0 || l == 1))
+					for (l = 0; l <= 9; l++)
 					{
-						if ((i == 0) && (j == 0) && (k == 0) && (l == 0))
-						{
-						}
-						else
-						{
-							putchar('0' + i);
-							putchar('0' + j);
-							putchar(32);
-							putchar('0' + k);
-							putchar('0' + l);
-							putchar(44);
-							putchar(32);
-						}
+						putchar('0' + i);
+						putchar('0' + j);
+						putchar(32);
+						putchar('0' + k);
+						putchar('0' + l);
+						putchar(44);
+						putchar(32);
 					}
 				}
 			}
-		}
+			else
+			{
+				for (k = i; k <= 9; k++)
+				{
+					for (l = j + 1; l <= 9; l++)
+					{
+						putchar('0' + i);
+						putchar('0' + j);
+						putchar(32);
+						putchar('0' + k);
+						putchar('0' + l);
+						putchar(44);
+						putchar(32);
+					}
+				}
+			}
 	}
-	putchar ('\n');
 	return (0);
 }
