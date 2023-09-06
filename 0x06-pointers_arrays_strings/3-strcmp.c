f24c0f794e72d06e4120f32620db082e116b2076
jarageda <jarageda9@gmail.com>
2023-09-06 09:23:13 +0000

    compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,7 +8,7 @@
   */
 int _strcmp(char *s1, char *s2)
 {
-	int i, num, j;
+	int i, j;
 
 	i = 0;
 	while (s1[i] != 0)
@@ -17,13 +17,11 @@ int _strcmp(char *s1, char *s2)
 		{
 			if (s1[i] > s2[j])
 			{
-				num = 15;
-				return (num);
+				return (s1[i] - s2[j]);
 			}
 			else if (s1[i] < s2[j])
 			{
-				num = -15;
-				return (num);
+				return (s1[i] - s2[j]);
 			}
 			else if (s1[i] == s2[j])
 				break;
