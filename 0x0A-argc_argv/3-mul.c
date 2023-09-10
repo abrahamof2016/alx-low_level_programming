bcbd09e912ff75b1299a853d85c6609cfe9d6e92
jarageda <jarageda9@gmail.com>
2023-09-10 12:36:56 +0000

    multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - writes product of two numbers into stdout.
+  * @argc: counts an argument.
+  * @argv: an array of pointers to char.
+  * Return: 0 on success.
+  */
+int main(int argc, char *argv[])
+{
+	int i;
+	int mul = 1;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul *= atoi(argv[i]);
+
+		}
+		printf("%d\n", mul);
+	}
+	else
+		printf("Error\n");
+	return (0);
+}
