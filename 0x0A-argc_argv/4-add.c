948ae46b2253bde42e9015f6c1a90800191c83bd
jarageda <jarageda9@gmail.com>
2023-09-10 13:12:58 +0000

    adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - adds numbers.
+  * @argc: counts an argument.
+  * @argv: an array of pointer to string.
+  * Return: 0 on success.
+  */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc == 1)
+		printf("%d\n", 0);
+	for (i = 1; i < argc; i++)
+	{
+		if ((*argv[i]) >= 48 && (*argv[i]) <= 57)
+		{
+			sum += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+
+
