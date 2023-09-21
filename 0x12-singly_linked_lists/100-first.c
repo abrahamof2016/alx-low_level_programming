5b3799e80d32d2651130ab9bc6977b11229298b2
jarageda <jarageda9@gmail.com>
2023-09-21 21:09:00 +0000

    writes a text

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+  * print - writes a text to stdout.
+  * Return: nothing.
+  **/
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
