cb6b9c6d3e82c903395ce71c7891b3110d8cf2b0
jarageda <jarageda9@gmail.com>
2023-09-26 20:26:30 +0000

    returns nth node.

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+  * get_nodeint_at_index - returns nth node.
+  * @head: a pointer.
+  * @index: a pointer.
+  * Return: pointer to a node.
+  */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *temp = head;
+
+	while (temp && i < index)
+	{
+		temp = temp->next;
+		i++;
+	}
+	return (temp ? temp : NULL);
+}
+
