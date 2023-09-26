9b8a0c0edcc24a409e7a21eb39cf2af65fcf28db
jarageda <jarageda9@gmail.com>
2023-09-26 12:25:47 +0000

    a structure definition

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,12 @@
+/**
+  * dog: a structure.
+  * @name: an argument of structure.
+  * @age: a float argument of structure.
+  * @owner: a character argument of structure.
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
