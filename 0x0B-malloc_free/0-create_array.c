98a12111bab6b4ccffe3b1dfb5dad0e08c64eac8
abrahamof2016 <jarageda9@gmail.com>
2024-01-17 22:06:59 +0000

    allocate memory

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,16 +1,22 @@
 #include <stdlib.h>
 /**
- * create_array - creates and initializes an array of char
- * @size: size of memory
- * @c: value stored
- * Return: memory address upon success
+ *create_array - creates and initializes an array of char
+ *@size: size of memory
+ *@c: value stored
+ *Return: memory address upon success
  */
 char *create_array(unsigned int size, char c)
 {
     unsigned int i;
     char *buffer;
 
+    if (size == 0)
+        return (NULL)
     buffer = malloc(sizeof(char) * 98);
+    if (buffer == NULL)
+    {
+        return (NULL)
+    }
     for (i = 0; i < size; i++)
     {
         buffer[i] = c;
