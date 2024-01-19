5f8ea21d1b1a18f8ffb0e7dd5511535593b5c3f2
abrahamof2016 <jarageda9@gmail.com>
2024-01-19 15:31:31 +0000

    point to newly allocated memory space

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include <stdlib.h>
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory.
+ * @str: string to be copied
+ * Return: a pointer to a newly allocated memory space
+ */
+char *_strdup(char *str)
+{
+	int len;
+	char *newspace;
+
+	len = sizeof(str);
+	if (str == NULL)
+		return (NULL);
+	newspace = malloc(sizeof(char) * len);
+	if (newspace == NULL)
+		return (NULL);
+	strcpy(newspace, str);
+	return (newspace);
+}
