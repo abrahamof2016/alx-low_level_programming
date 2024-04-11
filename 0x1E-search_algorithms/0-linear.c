7cf46e0745cda8e27829f36d8030f937fd0b1af5
abrehamof2016 <jarageda9@gmail.com>
2024-04-11 20:42:39 +0000

    use pointer to point to a value

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,9 +14,6 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if (array == NULL)
-		return (-1);
-
 	for (i = 0; i < size; i++)
 	{
 		printf("Value Checked arrary[%ld] = [%d]\n", i, array[i]);
